f2c9618c88ba5e118ed0782928cb18c9a9102a7c
Rajath <rrajath227@gmail.com>
2026-09-14 13:28:44 +0530

    feat: add User entity and AttemptResult enum

---
+++ src/main/java/com/dsa/assistant/model/Topic.java
@@ -0,0 +1,21 @@
+package com.dsa.assistant.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "topics")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
