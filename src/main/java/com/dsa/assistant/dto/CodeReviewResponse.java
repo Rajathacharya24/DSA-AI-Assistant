f7cd1647ae1640e50b8dff8484c4b1e22a480993
Rajath <rrajath227@gmail.com>
2026-09-19 19:57:52 +0530

    feat: add constructors and getters/setters to CodeReviewResponse

--- src/main/java/com/dsa/assistant/dto/CodeReviewResponse.java
+++ src/main/java/com/dsa/assistant/dto/CodeReviewResponse.java
@@ -15,4 +15,55 @@ public class CodeReviewResponse {
     private String timeComplexity;
     private String spaceComplexity;
     private String suggestions;
+
+    public CodeReviewResponse() {
+    }
+
+    public CodeReviewResponse(boolean correct, String feedback, String timeComplexity, String spaceComplexity, String suggestions) {
+        this.correct = correct;
+        this.feedback = feedback;
+        this.timeComplexity = timeComplexity;
+        this.spaceComplexity = spaceComplexity;
+        this.suggestions = suggestions;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getTimeComplexity() {
+        return timeComplexity;
+    }
+
+    public void setTimeComplexity(String timeComplexity) {
+        this.timeComplexity = timeComplexity;
+    }
+
+    public String getSpaceComplexity() {
+        return spaceComplexity;
+    }
+
+    public void setSpaceComplexity(String spaceComplexity) {
+        this.spaceComplexity = spaceComplexity;
+    }
+
+    public String getSuggestions() {
+        return suggestions;
+    }
+
+    public void setSuggestions(String suggestions) {
+        this.suggestions = suggestions;
+    }
 }
